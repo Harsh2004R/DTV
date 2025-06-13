c775cb40a323d14346141ec126b9129ba254bffe
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2025-06-13 11:04:29 -0500

    Typo mistake in PostMediaModel

--- server/Models/post.uploads.model.js
+++ server/Models/post.uploads.model.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-const Media = mongoose.Schema({
-    url: { require: true, type: String },
-    caption: { require: true, type: String },
-    likes: { type: Number },
-    comments: [],
-    uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }  // 👈 reffrence of which user upload post ?
-}, { timestamp: true });
-
+const Media = mongoose.Schema(
+    {
+        url: { required: true, type: String },
+        caption: { required: true, type: String },
+        likes: { type: Number, default: 0 },
+        comments: [],
+        uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true } // 👈 reffrence of which user uploaded which post ?
+    },
+    { timestamps: true }
+);
 const PostMediaModel = mongoose.model("post", Media);
-
 module.exports = { PostMediaModel };
